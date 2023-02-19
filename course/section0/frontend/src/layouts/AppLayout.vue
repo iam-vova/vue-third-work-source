1fcbef496cec778c545d8e8bd5e12faa3b7dcdba
iam-vova <iam-vova@yandex.ru>
2023-02-19 20:04:33 +0400

    1.1 | Добавляет компоненты AppLayout, AppLayoutHeader, HomeView

---
+++ course/section0/frontend/src/layouts/AppLayout.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="app_layout">
+        <AppLayoutHeader />
+        <slot/>
+    </div>
+</template>
+
+<script>
+import AppLayoutHeader from '@/layouts/AppLayoutHeader.vue';
+
+export default {
+    name: "AppLayout",
+    components: {AppLayoutHeader}
+}
+</script>
