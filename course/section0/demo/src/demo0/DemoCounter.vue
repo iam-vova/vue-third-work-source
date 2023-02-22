85fef82b6d16c44fb5013abad84584cc8ee4951e
iam-vova <iam-vova@yandex.ru>
2023-02-22 21:57:28 +0400

    1.7 | Добавляет стили в демо-счетчик

--- course/section0/demo/src/demo0/DemoCounter.vue
+++ course/section0/demo/src/demo0/DemoCounter.vue
@@ -9,15 +9,38 @@
 </template>
 
 <script setup>
-import { ref } from 'vue'
+    import { ref } from 'vue';
 
-const count = ref(0)
+    const count = ref(0);
 
-const decrement = () => {
-    count.value = count.value - 1
-}
+    const decrement = () => {
+        count.value -= 1;
+    };
 
-const increment = () => {
-    count.value = count.value + 1
-}
+    const increment = () => {
+        count.value += 1;
+    };
 </script>
+
+<style lang="scss" scoped>
+    .counter {
+        display: flex;
+        align-items: center;
+
+        button {
+            margin: 0 10px;
+            font-size: 1.5rem;
+            width: 2rem;
+            height: 2rem;
+            border-radius: 50%;
+            border: none;
+            background: #dd0000;
+            color: white;
+            cursor: pointer;
+
+            &:hover {
+                background: #ff0000;
+            }
+        }
+    }
+</style>
