57a4e53d537c7f575d08ce6e13b9325ef2e81613
iam-vova <iam-vova@yandex.ru>
2023-02-24 22:45:59 +0400

    1.9 | Добавляет стили в компоненты

--- course/section0/frontend/src/App.vue
+++ course/section0/frontend/src/App.vue
@@ -8,3 +8,7 @@
     import AppLayout from '@/layouts/AppLayout.vue';
     import HomeView from '@/views/HomeView.vue';
 </script>
+
+<style lang="scss">
+    @import "@/assets/scss/app.scss";
+</style>
